ced9784ca61b5ef0355240df61c1d7625028a9ee
btmxh <ngoduyanh.chip@gmail.com>
2025-01-01 22:10:51 +0700

    test: add favicon test

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -5,3 +5,20 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/plst4/);
 });
 
+test('has favicon', async ({ page }) => {
+  await page.goto("/");
+  const iconLink = await page.$("link[rel='icon']");
+  expect(iconLink).not.toBeNull();
+
+  const iconHref = await iconLink?.getAttribute('href');
+  expect(iconHref).not.toBeNull();
+
+  const iconUrl = new URL(iconHref!, page.url()).toString();
+
+  const [faviconResponse] = await Promise.all([
+    page.waitForResponse(iconUrl),
+    page.goto("/"),
+  ]);
+
+  expect(faviconResponse.status()).toBe(200);
+})
