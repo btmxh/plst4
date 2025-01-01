8d9d8084217cd934358fbbc226dad4e8b853b72f
btmxh <ngoduyanh.chip@gmail.com>
2025-01-01 21:59:34 +0700

    fix: use MAIL_MODE env to pick mail mode

--- internal/mailer/interface.go
+++ internal/mailer/interface.go
@@ -1,8 +1,10 @@
 package mailer
 
 import (
+	"fmt"
 	"html/template"
 	"net/mail"
+	"os"
 	"strings"
 )
 
@@ -24,4 +26,21 @@ func SendMailTemplated(to *mail.Address, subject string, tmpl *template.Template
 	}
 
 	return DefaultMailer.SendMail(to, subject, template.HTML(writer.String()))
+}
+
+func InitMailer() error {
+	mode := os.Getenv("MAIL_MODE")
+	if mode == "" {
+		mode = "netmail"
+	}
+
+	switch mode {
+	case "netmail":
+		return InitNetMailer()
+	case "memorymail":
+		InitMemoryMailer()
+		return nil
+	default:
+		panic(fmt.Sprintf("Invalid mail mode: %s", mode))
+	}
 }
